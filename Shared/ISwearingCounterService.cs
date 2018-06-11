0ed53f3d31a5bd3dd16aac74cbe54e5aafcd4a97
AJ <ales.jaterka@fullsys.cz>
2018-06-11 06:24:50 +0200

    Založení projektů a repozitáře.

--- Shared/ISwearingCounterService.cs
+++ Shared/ISwearingCounterService.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 
-namespace SwearingCounterService
+namespace SwearingCounter.Shared
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISwearingCounterService" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface ISwearingCounterService
     {
 
         [OperationContract]
---
+++ SwearingCounter/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+using SwearingCounter.Shared;
+
+namespace SwearingCounter
+{
+    public class MainViewModel
+    {
+        ServiceHost m_host = new ServiceHost(typeof(ISwearingCounterService));
+
+        public void StartHost()
+        {
+            m_host.Open();
+        }
+
+        public void StopHost()
+        {
+            m_host.Close();
+        }
+    }
+}
